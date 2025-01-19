c93f0e488bb4ddf9cb2b2a6708bc962965c85562
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-01-19 17:36:53 -0400

    chore: init nix flakes

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+      fenix = inputs.fenix.packages;
+    in
+    # Iterate over Arm, x86 for MacOs 🍎 and Linux 🐧
+    (flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        crane = inputs.crane.mkLib pkgs;
+        # Toolchain
+        toolchain = fenix.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+        };
+        craneLib = crane.overrideToolchain toolchain;
+
+        buildInputs = with pkgs; [
+          openssl.dev
+          pkg-config
+          wayland
+
+          linuxHeaders
+          libv4l
+          glibc.dev
+        ];
+
+        nativeBuildInputs = [
+          pkgs.llvmPackages_16.libstdcxxClang
+          pkgs.clang-tools
+        ];
+
+        commonArgs = {
+          doCheck = false;
+          src = craneLib.path ./.;
+          inherit buildInputs nativeBuildInputs;
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          # CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+        };
+
+        libraries = with pkgs; [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+        # Compile all artifacts
+        appDeps = craneLib.buildDepsOnly commonArgs;
+
+        # Compile
+        app = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = appDeps;
+        });
+      in {
+        # nix build
+        packages.default = app;
+
+        # nix run
+        apps.default = flake-utils.lib.mkApp {
+          drv = app;
+        };
+
+        # nix develop
+        devShells.default = craneLib.devShell {
+          inherit buildInputs nativeBuildInputs;
+
+          packages = with pkgs; [
+            toolchain
+            heaptrack
+          ];
+
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+        };
+      }
+    ));
+}
